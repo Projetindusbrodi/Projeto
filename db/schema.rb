0f260ddbfb8e278a6a27cbbc119998954f0b5ac0
Raquel <raquel.queiroz@youse.com.br>
2017-05-04 17:01:49 -0300

    Criando tabela de notificações e o model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504164416) do
+ActiveRecord::Schema.define(version: 20170504195815) do
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer  "produto_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["produto_id"], name: "index_notifications_on_produto_id"
+    t.index ["user_id"], name: "index_notifications_on_user_id"
+  end
 
   create_table "order_items", force: :cascade do |t|
     t.integer  "produto_id"
