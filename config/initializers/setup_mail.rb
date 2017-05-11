3cbc81baa7450f9e9145ea71a2259ebfdc8f8fb9
Projetindusbrodi <projetindusbrodi@gmail.com>
2017-05-10 23:39:50 -0300

    Update setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,3 @@
 
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.mandrillapp.com",
-  :port                 => 587,
-  :domain               => "youse.com.br",
-  :user_name            => "suptools@youse.com.br'",
-  :password             => "MKclzv5rdC7bTK7E_UHDfQ",
-  :authentication       => 'login',
-  :enable_starttls_auto => true
-}
 
 # ActionMailer::Base.default_url_options[:host] = "localhost:3000"
